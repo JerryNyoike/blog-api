c13147a4ef452fc4bf9f8d298f18c6821ec06262
Jerry Nyoike <jngugi31@gmail.com>
2022-03-10 10:12:31 +0300

    Add routes for users and comments

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ app.use(bodyParser.json()); // use bodyparser to parse json body
 
 // Routing
 const postsRoute = require('./routes/posts');
+const usersRoute = require('./routes/users');
+const commentsRoute = require('./routes/comments');
 
 // can add routes as if they were middleware
 app.use('/posts', postsRoute);
+app.use('/users', usersRoute);
+app.use('/comments', commentsRoute);
 
 // database connection
 mongoose.connect(process.env.DB_URL, {useNewURLParser: true}, () => {
     console.log('connected to DB!');
 });
 
-// Routes
-app.get('/', (req, res) => {
-    res.send("This is home");
-});
 
 // start server
 app.listen(process.env.PORT);
