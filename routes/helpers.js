a2cd8606ae8efaa94cc2eae796b258fc4dbb4c80
Jerry Nyoike <jngugi31@gmail.com>
2022-03-10 21:47:39 +0300

    Generate keys for signing JWT tokens using the crypto libraries

--- routes/helpers.js
+++ routes/helpers.js
@@ -1,21 +1,21 @@
+const crypto = require('crypto');
+const webcrypto = require('webcrypto');
 const bcrypt = require('bcrypt');
+const jose = require('jose');
+require('dotenv').config();
 
-async function encrypt_pass() {
-    const pass_salt = bcrypt.salt(10);
-
-    return await bcrypt.hash(password, pass_salt());
+async function encrypt_pass(password) {
+    return await bcrypt.hash(password, process.env.HASH_ROUNDS);
 }
 
-async function loginToken(user) {
+async function loginToken(user) {    
     try {
-	const token = await jose.SignJWT()
-	      .setProtectedHeader({})
-	      .setIssuedAt(Date.now())
-	      .setAudience(user._id)
-	      .setExpirationTime('2d')
-	      .sign(process.env.KEY)
-
-	return token;
+	return await new jose.SignJWT({})
+	    .setProtectedHeader({ alg: 'HS256' })
+	    .setIssuedAt(Date.now())
+	    .setAudience(user._id)
+	    .setExpirationTime('2h')
+	    .sign(await crypto.createSecretKey(process.env.KEY));
     } catch (err) {
 	return null;
     }
@@ -23,11 +23,12 @@ async function loginToken(user) {
 
 async function loggedIn(token) {
     try{
-	const { payload, header } = await jose.jwtVerify(token, process.env.KEY);
+	const { payload, header } = await jose.jwtVerify(token, await crypto.createSecretKey(process.env.KEY));
+	console.log(`Payload: ${payload}`);
 	return { loggedIn: payload.aud == user._id, u_id: payload.aud };
     } catch (err) {
-	return false;
+	return { loggedIn: false, u_id: null };
     }
 }
 
-module.exports = { encrypt_pass, loginToken };
+module.exports = { encrypt_pass, loginToken, loggedIn };
