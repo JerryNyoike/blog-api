b4416a1cf2443c19625e38ce010526b61326c84a
JerryNyoike <jngugi31@gmail.com>
2022-03-20 04:03:25 +0300

    Use jsonwebtoken library instead of jose

--- routes/helpers.js
+++ routes/helpers.js
@@ -1,31 +1,24 @@
 const crypto = require('crypto');
 const webcrypto = require('webcrypto');
 const bcrypt = require('bcrypt');
-const jose = require('jose');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 async function encrypt_pass(password) {
     return await bcrypt.hash(password, process.env.HASH_ROUNDS);
 }
 
-async function loginToken(user) {    
-    try {
-	return await new jose.SignJWT({})
-	    .setProtectedHeader({ alg: 'HS256' })
-	    .setIssuedAt(Date.now())
-	    .setAudience(user._id)
-	    .setExpirationTime('2h')
-	    .sign(await crypto.createSecretKey(process.env.KEY));
-    } catch (err) {
-	return null;
-    }
+async function loginToken(user) {
+    return jwt.sign({ data: user._id },
+		    process.env.KEY,
+		    { expiresIn: '7d' });
 }
 
 async function loggedIn(token) {
     try{
-	const { payload, header } = await jose.jwtVerify(token, await crypto.createSecretKey(process.env.KEY));
-	console.log(`Payload: ${payload}`);
-	return { loggedIn: payload.aud == user._id, u_id: payload.aud };
+	const data = jwt.verify(token, process.env.KEY);
+	
+	return { loggedIn: payload.data._id == user._id, u_id: payload.data._id };
     } catch (err) {
 	return { loggedIn: false, u_id: null };
     }
