0018ca942ffa3a84e1a29f7b82b6ef58daffc6aa
JerryNyoike <jngugi31@gmail.com>
2022-03-20 05:21:36 +0300

    Add a content field in the Post model

--- models/Post.js
+++ models/Post.js
@@ -9,6 +9,10 @@ const PostSchema = new mongoose.Schema({
 	type: String,
 	required: true
     },
+    content: {
+	type: String,
+	required: true
+    },
     date: {
 	type: Date,
 	default: Date.now,
