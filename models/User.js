68f7fb9a30ddbad2c18909b0aa6b048aa7bb6ae2
JerryNyoike <jngugi31@gmail.com>
2022-03-20 04:02:50 +0300

    Add unique constraint to User model

--- models/User.js
+++ models/User.js
@@ -1,18 +1,23 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const UserSchema = new mongoose.Schema({
     username: {
 	type: String,
-	required: true
+	required: true,
+	unique: true
     },
     email: {
 	type: String,
-	required: true
+	required: true,
+	unique: true
     },
     password: {
 	type: String,
 	required: true
     }
 });
+
+UserSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('User', UserSchema);
